ba2e5d0ed9cb33e2b1cadc46eb4e7c38d9213cd8
Palmer-Hatch <56094431+palmermexico@users.noreply.github.com>
2022-03-04 10:05:14 -0700

    prob 1

--- server/index.js
+++ server/index.js
@@ -22,4 +22,19 @@ app.get("/api/compliment", (req, res) => {
   
 });
 
+
+app.get("/api/compliment", (req, res) => {
+  const compliments = ["Gee, you're a smart cookie!",
+					 "Cool shirt!",
+					 "Your Javascript skills are stellar.",
+  ];
+
+  // choose random compliment
+  let randomIndex = Math.floor(Math.random() * compliments.length);
+  let randomCompliment = compliments[randomIndex];
+
+  res.status(200).send(randomCompliment);
+  
+});
+
 app.listen(4000, () => console.log("Server running on 4000"));
